7310734349ca9de14c2a8660b4490c6a541b089a
Asraf Ali <asraf.cse@gmail.com>
2020-12-14 16:28:35 +0530

    Get data from nseindia

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,27 @@
-import Calculation from './index'
-describe('class: Caluculation', () => {
-    const calc = new Calculation()
-    const a = 10
-    test('add()', () => {
-        expect(calc.add(a, 20)).toBe(30)
+import NseIndia, { ApiList } from "./index";
+
+jest.setTimeout(999999)
+
+const sleep = (ms: number) => {
+    return new Promise<void>(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
     })
-    test('subtract()', () => {
-        expect(calc.subtrct(a, 5)).toBe(5)
+}
+
+describe('class: NseIndia', () => {
+    const nseIndia = new NseIndia()
+    test('getAllStockSymbols', async () => {
+        const symbols = await nseIndia.getAllStockSymbols()
+        expect(symbols.length).toBeGreaterThan(1000)
+    })
+
+    Object.values(ApiList).forEach(apiEndpoint => {
+        test(`should return content for ${apiEndpoint}`, async () => {
+            const data = await nseIndia.getDataByEndpoint(apiEndpoint)
+            const contentLength = JSON.stringify(data).length
+            expect(contentLength).not.toBe(0)
+        })
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
-import { EventEmitter } from 'events'
-export default class Calculation extends EventEmitter {
-    add(a: number, b: number): number {
-        this.emit('done', 'addition')
-        return a + b
+import axios from 'axios'
+
+export enum ApiList {
+    HOLIDAY_TRADING = '/api/holiday-master?type=trading',
+    HOLIDAY_CLEARING = '/api/holiday-master?type=clearing',
+    MARKET_STATUS = '/api/marketStatus',
+    MARKET_TURNOVER = '/api/market-turnover',
+    INDEX_NAMES = '/api/index-names',
+    CIRCULARS = '/api/circulars',
+    LATEST_CIRCULARS = '/api/latest-circular',
+    EQUITY_MASTER = '/api/equity-master'
+}
+export default class NseIndia {
+    private baseUrl = 'https://www.nseindia.com'
+    private cookies = ''
+    private cookieUsedCount = 0
+    private cookieMaxAge = 60 // should be in seconds
+    private cookieExpiry = new Date().getTime() + (this.cookieMaxAge * 1000)
+
+    private async getNseCookies() {
+        if (this.cookies === '' || this.cookieUsedCount > 10 || this.cookieExpiry <= new Date().getTime()) {
+            const response = await axios.get(this.baseUrl)
+            const setCookies = response.headers['set-cookie']
+            const cookies: string[] = []
+            setCookies.forEach((cookie: string) => {
+                const requiredCookies: string[] = ['nsit', 'nseappid']
+                const cookieKeyValue = cookie.split(';')[0]
+                const cookieEntry = cookieKeyValue.split('=')
+                if (requiredCookies.includes(cookieEntry[0])) {
+                    cookies.push(cookieKeyValue)
+                }
+            })
+            this.cookies = cookies.join('; ')
+            this.cookieUsedCount = 0
+            this.cookieExpiry = new Date().getTime() + (this.cookieMaxAge * 1000)
+        }
+        this.cookieUsedCount++
+        return this.cookies
     }
-    subtrct(a: number, b: number): number {
-        this.emit('done', 'subtraction')
-        return a - b
+    async getAllStockSymbols() {
+        try {
+            const responseData = await this.getData(`${this.baseUrl}/api/market-data-pre-open?key=ALL`)
+            return responseData.data.map((obj: { metadata: { symbol: string } }) => obj.metadata.symbol).sort()
+        } catch (error) {
+            Promise.reject(error)
+        }
+    }
+    private async getData(url: string) {
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    'Cookie': await this.getNseCookies()
+                }
+            })
+            return response.data
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    }
+    async getDataByEndpoint(apiEndpoint: string) {
+        try {
+            return await this.getData(`${this.baseUrl}${apiEndpoint}`)
+        } catch (error) {
+            Promise.reject(error)
+        }
     }
 }
