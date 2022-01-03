0c5532272365d7fd5d9977216c515be7a9bb550d
Asraf Ali <asraf.cse@gmail.com>
2022-01-03 21:25:51 +0530

    feat: added gainersAndLosers helper methods

---
+++ src/helpers.spec.ts
@@ -0,0 +1,20 @@
+import {
+    getGainersAndLosersByIndex,
+    getMostActiveEquities
+} from './helpers'
+
+describe('Helpers', () => {
+    test('getGainersAndLosersByIndex', async () => {
+        const data = await getGainersAndLosersByIndex("NIFTY 50")
+        const isLosers = data.losers.every((equityDetails) => equityDetails.pChange <= 0)
+        const isGainers = data.gainers.every((equityDetails) => equityDetails.pChange > 0)
+        expect(isLosers).toBeTruthy()
+        expect(isGainers).toBeTruthy()
+    })
+
+    test('getMostActiveEquities', async () => {
+        const data = await getMostActiveEquities("NIFTY 50")
+        expect(data.byVolume[0] >= data.byVolume[1]).toBeTruthy()
+        expect(data.byValue[0] >= data.byValue[1]).toBeTruthy()
+    })
+})
---
+++ src/helpers.ts
@@ -0,0 +1,30 @@
+import { ApiList, NseIndia } from './index'
+import { IndexEquityInfo } from './interface'
+
+const nseIndia = new NseIndia()
+
+export const getGainersAndLosersByIndex = async (indexSymbol: string) => {
+    const indexData = await nseIndia.getEquityStockIndices(indexSymbol)
+    const gainers: IndexEquityInfo[] = []
+    const losers: IndexEquityInfo[] = []
+    indexData.data.forEach((equityInfo: IndexEquityInfo) => {
+        if (equityInfo.pChange > 0)
+            gainers.push(equityInfo)
+        else
+            losers.push(equityInfo)
+    })
+    return {
+        gainers: gainers.sort((a, b) => b.pChange - a.pChange),
+        losers: losers.sort((a, b) => a.pChange - b.pChange)
+    }
+}
+
+export const getMostActiveEquities = async (indexSymbol: string) => {
+    const indexData = await nseIndia.getEquityStockIndices(indexSymbol)
+    return {
+        byVolume: indexData.data.sort((a, b) => b.totalTradedVolume - a.totalTradedVolume),
+        byValue: indexData.data.sort((a, b) => b.totalTradedValue - a.totalTradedValue)
+
+    }
+}
+
