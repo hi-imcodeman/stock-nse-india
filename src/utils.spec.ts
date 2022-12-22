99d925e25983ed0eecdafdd9e690781c9d1e590f
Asraf Ali <asraf.cse@gmail.com>
2022-12-22 20:04:57 +0530

    Disable strict api validation (#54)
    * chore: disable stric api validation
    
    * chore: coverage fix

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,5 +1,16 @@
 import { getDataSchema } from './utils'
 describe('Utils', () => {
+    test('getDataSchema with type strict',()=>{
+        const data={
+            a:1,
+            b: 'b',
+            c: [1,2,3],
+            d: {a:1,b:[2,3,4],c:{d:1}},
+            e: new Date('2022-09-12')
+        }
+        expect(getDataSchema(data)).toMatchSnapshot()
+        expect(getDataSchema(['hi'])).toMatchSnapshot('array_validation')
+    })
     test('getDataSchema for date value having typeStrict:false',()=>{
         const data={
             date: new Date('2022-09-12')
