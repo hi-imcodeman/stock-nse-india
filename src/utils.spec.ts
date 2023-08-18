d39da944b1adaabea2b137494fa8896b6f78adc2
Asraf Ali <asraf.cse@gmail.com>
2023-08-18 13:08:35 +0530

    chore: removed api validation on helpers

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,22 +1,19 @@
 import { getDataSchema } from './utils'
 describe('Utils', () => {
+    const data={
+        a: 1,
+        b: 'b',
+        c: [1,2,3],
+        d: {a:1,b:[2,3,4],c:{d:1},e:[]},
+        e: new Date('2022-09-12'),
+        f: [[1,2],[3,4]]
+    }
     test('getDataSchema with type strict',()=>{
-        const data={
-            a: 1,
-            b: 'b',
-            c: [1,2,3],
-            d: {a:1,b:[2,3,4],c:{d:1}},
-            e: new Date('2022-09-12'),
-            f: [[1,2],[3,4]]
-        }
         expect(getDataSchema(data)).toMatchSnapshot()
         expect(getDataSchema(['hi'])).toMatchSnapshot('array_validation')
     })
     test('getDataSchema for date value having typeStrict:false',()=>{
-        const data={
-            date: new Date('2022-09-12'),
-            arrayOfArray: [[1,2],[3,4]]
-        }
         expect(getDataSchema(data,false)).toMatchSnapshot()
+        expect(getDataSchema(['hi'],false)).toMatchSnapshot('array_validation')
     })
 })
